4635611d80b68cfbc07c2d7e94262d4508f5217b
dfateev <dfateev@team.amocrm.com>
2023-04-17 18:42:25 +0300

    Create variables crate

---
+++ variables/target/debug/deps/variables-0f34dc8ae57f2e74.d
@@ -0,0 +1,5 @@
+/home/dfateev/Work/MY_PROJ/Rust_Ex/variables/target/debug/deps/variables-0f34dc8ae57f2e74: src/main.rs
+
+/home/dfateev/Work/MY_PROJ/Rust_Ex/variables/target/debug/deps/variables-0f34dc8ae57f2e74.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/dfateev/Work/MY_PROJ/Rust_Ex/variables/target/debug/variables: /home/dfateev/Work/MY_PROJ/Rust_Ex/variables/src/main.rs
