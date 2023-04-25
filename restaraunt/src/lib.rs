373885b086e002c2ae051610c54ef83c436d192a
dfateev <dfateev@team.amocrm.com>
2023-04-25 18:46:35 +0300

    Learning modules

---
+++ restaraunt/src/lib.rs
@@ -0,0 +1,27 @@
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        season_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast { 
+                toast: String::from(toast), 
+                season_fruit: String::from("peaches") 
+            }
+        }
+    }
+}
+/*
+    В рамках общего крейта back_of_house является доступным модулем
+    Для структуры и методов внутри нее необходимо указывать доступность ключевым словом pub
+    В частности для работы с внешними методами доступно только одно поле структуры - toast
+    Метод структу
+*/
+
+pub fn eat_at_restaraunt() {
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("Id liken {} toast please", meal.toast);
+}
